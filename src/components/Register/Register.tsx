217ee3d654567a4440ca9b7ccff4209dadd8acde
ozkavosh <augustoslva@hotmail.com>
2022-10-20 12:59:28 -0300

    Register Component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,9 +1,40 @@
-import './Register.scss'
+import "./Register.scss";
+import { AiOutlineUser, AiFillLock, AiOutlineMail, AiFillGoogleCircle } from 'react-icons/ai';
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <main className="register">
+      <div className="container">
+        <form className="registerForm">
+          <h2>Registrar nuevo usuario</h2>
+          <div className="formInput">
+            <AiOutlineUser/>
+            <input type="text" name="displayName" placeholder="Ingresar nombre de usuario..." />
+          </div>
+          <div className="formInput">
+            <AiOutlineMail/>
+            <input type="email" name="email" placeholder="Ingresar correo..." />
+          </div>
+          <div className="formInput">
+            <AiFillLock/>
+            <input type="password" name="password" placeholder="Ingresar contraseña..." />
+          </div>
 
-export default Register
+          <Link to="/login" className="loginLink">Ya tengo cuenta</Link>
+
+          <button className="submit">Registrar</button>
+
+          <div className="networkLogin">
+                    <h3 className="networkLoginHeading">o bien</h3>
+                    <AiFillGoogleCircle/>
+          </div>
+
+          <Link to="/ayuda" className="policyLink">Política de privacidad</Link>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
